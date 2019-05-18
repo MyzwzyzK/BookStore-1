e851a3547596f7396f6a90f153d48e94d08cd946
binshaos <18670688803@163.com>
2019-05-18 09:23:22 +0800

    BookStore
    Change-Id: I1b5cd6c1654587b58e01afab1afb27f2db1db7d2
    Signed-off-by: MyzwzyzK <50611268+MyzwzyzK@users.noreply.github.com>

---
+++ src/main/java/com/jp/controller/AdminController.java
@@ -0,0 +1,9 @@
+package com.jp.controller;
+/**
+ * 	测试HitHub上传
+ * @author 张永政
+ *
+ */
+public class AdminController {
+
+}
---
+++ src/main/java/com/jp/interceptor/AuthorizationInterceptor.java
@@ -0,0 +1,5 @@
+package com.jp.interceptor;
+
+public class AuthorizationInterceptor {
+
+}
